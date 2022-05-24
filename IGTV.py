3e94b8c0b833a55c5aca2821d16d411f9e0c1306
Charlocco <yuruyuritaira@gmail.com>
2022-05-24 15:22:10 +0900

    first commit

---
+++ IGTV.py
@@ -0,0 +1,25 @@
+import pathlib
+import os
+from Class_Instagram import Directory_Class, Instagram
+#import Local_Instagram.Class_Instagram
+from win32com.propsys import propsys, pscon
+from win32com.shell import shellcon
+
+directory = Directory_Class()
+now_directory = os.path.dirname(__file__)
+present_name = directory.User_name
+file_name = directory.Directory_Name[3]
+path = os.path.join(now_directory, present_name)
+path = os.path.join(path, file_name)
+data = Instagram(path)
+
+def run():
+    directory.Make_Directory()
+    data.Insta()
+    data.Login()
+    data.Get_Value()
+    data.IGTV_Post()
+    directory.Delete_Directory()
+
+if __name__ == '__main__':
+    app = run()
